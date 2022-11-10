5899480b2dfa2253871d3fc4b9c55b3e73cf749a
Lada <100614341+ladaln@users.noreply.github.com>
2022-11-10 14:25:42 +0300

    add: task 2

---
+++ 03-files-in-folder/form.js
@@ -0,0 +1,10 @@
+exports.formatBytes = (a, b = 2) => {
+  if (!+a) return '0 Bytes'
+
+  const c = 0 > b ? 0 : b
+  const d = Math.floor(Math.log(a) / Math.log(1024))
+
+  return `${parseFloat((a / Math.pow(1024, d)).toFixed(c))} ${
+    ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'][d]
+  }`
+}
